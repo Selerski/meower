232b9d914204656af8b1816491ce311618ae3e24
Pawel <pawel.pietruszka99@gmail.com>
2020-02-02 15:49:52 +0000

    twitter

--- client/client.js
+++ client/client.js
@@ -1,117 +1,66 @@
-console.log('Hello World!');
-
-const form = document.querySelector('form'); // grabbing an element on the page
-const errorElement = document.querySelector('.error-message');
+const form = document.querySelector('form');
+const tweetElement = document.querySelector('.tweets');
 const loadingElement = document.querySelector('.loading');
-const mewsElement = document.querySelector('.mews');
-const loadMoreElement = document.querySelector('#loadMore');
-const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/v2/mews' : 'https://meower-api.now.sh/v2/mews';
+loadingElement.style.display = 'none';
+const API_URL = 'http://localhost:3000/tweets';
 
-let skip = 0;
-let limit = 5;
-let loading = false;
-let finished = false;
+listAllTweets();
 
-errorElement.style.display = 'none';
-
-document.addEventListener('scroll', () => {
-  const rect = loadMoreElement.getBoundingClientRect();
-  if (rect.top < window.innerHeight && !loading && !finished) {
-    loadMore();
-  }
-});
-
-listAllMews();
-
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const formData = new FormData(form);
   const name = formData.get('name');
   const content = formData.get('content');
 
-  if (name.trim() && content.trim()) {
-    errorElement.style.display = 'none';
-    form.style.display = 'none';
-    loadingElement.style.display = '';
+  const tweet = {
+    name,
+    content
+  };
 
-    const mew = {
-      name,
-      content
-    };
-    
-    fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify(mew),
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => {      
-      if (!response.ok) {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('json')) {
-          return response.json().then(error => Promise.reject(error.message));
-        } else {
-          return response.text().then(message => Promise.reject(message));
-        }
-      }
-    }).then(() => {
+  form.style.display = 'none';
+  loadingElement.style.display = '';
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(tweet),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
       form.reset();
-      setTimeout(() => {
-        form.style.display = '';
-      }, 30000);
-      listAllMews();
-    }).catch(errorMessage => {
       form.style.display = '';
-      errorElement.textContent = errorMessage;
-      errorElement.style.display = '';
+      listAllTweets();
       loadingElement.style.display = 'none';
     });
-  } else {
-    errorElement.textContent = 'Name and content are required!';
-    errorElement.style.display = '';
-  }
 });
 
-function loadMore() {
-  skip += limit;
-  listAllMews(false);
-}
-
-function listAllMews(reset = true) {
-  loading = true;
-  if (reset) {
-    mewsElement.innerHTML = '';
-    skip = 0;
-    finished = false;
-  }
-  fetch(`${API_URL}?skip=${skip}&limit=${limit}`)
+function listAllTweets() {
+  tweetElement.innerHTML = '';
+  fetch(API_URL)
     .then(response => response.json())
-    .then(result => {
-      result.mews.forEach(mew => {
+    .then(tweets => {
+      tweets.reverse()
+      tweets.forEach(tweet => {
         const div = document.createElement('div');
 
         const header = document.createElement('h3');
-        header.textContent = mew.name;
+        header.textContent = tweet.name;
 
         const contents = document.createElement('p');
-        contents.textContent = mew.content;
+        contents.textContent = tweet.content;
 
-        const date = document.createElement('small');
-        date.textContent = new Date(mew.created);
+        const date = document.createElement('small')
+        date.textContent = tweet.created;
 
         div.appendChild(header);
         div.appendChild(contents);
         div.appendChild(date);
 
-        mewsElement.appendChild(div);
+        tweetElement.appendChild(div);
       });
-      loadingElement.style.display = 'none';
-      if (!result.meta.has_more) {
-        loadMoreElement.style.visibility = 'hidden';
-        finished = true;
-      } else {
-        loadMoreElement.style.visibility = 'visible';
-      }
-      loading = false;
     });
 }
